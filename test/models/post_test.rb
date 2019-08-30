0c0b61ebdbf5b6351ec371abd2bebb2573c6f7ce
fatima ahmed <fatymah001@gmail.com>
2019-08-30 23:02:27 +0300

    Fix stickler errors

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
@@ -5,21 +7,21 @@ class PostTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @post=Post.new(link:"hey", user_id:1)
+    @post = Post.new(link: 'hey', user_id: 1)
   end
 
   test "post should'nt be empty" do
-    @post.link=""
+    @post.link = ''
     assert_not @post.valid?
   end
 
-  test "post should have a user id " do
-    @post.user_id=nil
+  test 'post should have a user id ' do
+    @post.user_id = nil
     assert_not @post.valid?
   end
 
-  test "post shouldnt be more than 140 chars" do
-    @post.link="a"*141
+  test 'post shouldnt be more than 140 chars' do
+    @post.link = 'a' * 141
     assert_not @post.valid?
   end
 end
